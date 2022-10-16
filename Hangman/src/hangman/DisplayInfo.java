783ac1b7bb6f0acc29cb98dc42b6dd7d3dbcacd8
Simon Nguyen <Simon Nguyen@10.110.211.112>
2022-10-15 19:31:27 -0700

    Changed F1 key to output a dialog box instead

--- Hangman/src/hangman/DisplayInfo.java
+++ Hangman/src/hangman/DisplayInfo.java
@@ -15,17 +15,35 @@ package hangman;
 
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
 
 /**
  *
  * @author Simon Nguyen
  */
 public class DisplayInfo extends AbstractAction {
+    //InfoScreen temp;
+    JFrame frame;
 
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        InfoScreen temp = new InfoScreen();
-        temp.show();
+        //temp = new InfoScreen();
+        
+        frame = new JFrame();
+        
+        String [] names = { "Term: Fall 2022\n\nCredits:",
+                            "Dixon Dai, 014899781", 
+                            "Shane Baxter, 013655473", 
+                            "Ethan Melero, 014669369",
+                            "Marvin Sevilla, 014646346",
+                            "Simon Nguyen, 016372824"};
+        String title = "Project: Point and Click Game - v1.2";
+
+        //Test Later
+        JOptionPane.showMessageDialog(frame, names, title, JOptionPane.PLAIN_MESSAGE);
+        
+        //temp.setVisible(true);
     }
 }
